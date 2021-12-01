903d2d92153567e54d498345d2dc2ef6802b98a7
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2021-12-01 09:45:23 -0500

    Update R_credentials_sample.R

--- R_credentials_sample.R
+++ R_credentials_sample.R
@@ -1,5 +1,5 @@
-# A place to store your username and password.  
-# To use this:
+#  A place to store your username and password.  
+#  To use this:
 #    make a copy of this file.
 #    Name the copy "R_credentials.R"
 #    Place it in your root directory.  
@@ -7,6 +7,7 @@
 
 #  Depending on how the rest of your system is configured, you may find it useful to fill in the rest of the connection details here (like the path to the server, port, ssid).
 
+# "R_credentials.R" has been added to .gitignore, but verify that actually has happened before the first commit.
 
 
 
