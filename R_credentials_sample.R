85b1405b951c2d971b911f0735b3da0d8475ce2f
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2021-12-01 09:44:16 -0500

    Add instruction on how to use.

--- R_credentials_sample.R
+++ R_credentials_sample.R
@@ -1,3 +1,16 @@
+# A place to store your username and password.  
+# To use this:
+#    make a copy of this file.
+#    Name the copy "R_credentials.R"
+#    Place it in your root directory.  
+#    Fill in your oracle_username, oracle_password.  
+
+#  Depending on how the rest of your system is configured, you may find it useful to fill in the rest of the connection details here (like the path to the server, port, ssid).
+
+
+
+
+
 oracle_server = "sole"
 
 oracle_username<-"<yourid>"
