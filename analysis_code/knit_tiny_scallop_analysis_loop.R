20d3f71b947f528412615a9fdd04c8b6c7657857
mle2718 <Min-Yang.Lee@noaa.gov>
2022-12-08 08:27:43 -0500

    split the tiny scallop analysis from the main scallop analysis.

--- analysis_code/knit_tiny_scallop_analysis_loop.R
+++ analysis_code/knit_tiny_scallop_analysis_loop.R
@@ -9,10 +9,10 @@ here::i_am("analysis_code/knit_scallop_analysis_loop.R")
 # Define the "things" you want to loop over.
 AADAS_Vals_to_loop<-c(TRUE) # TRUE is the AADAS fleet. FALSE is the IFQ fleet
 subtrip_aggregate_vals_to_loop<-c(TRUE) # TRUE aggregates subtrips to trips. FALSE does not
-input_shapefile_vals_to_loop<-c("wind_NY2.RDS", "wind_sf_final.RDS", "wind_sf_nocable.RDS") # Different "closure areas". 
 
-
-# You shouldn't need to edit anything below this.
+# We want to be able to run the sc
+# input_shapefile_vals_to_loop2<-c("wind_NY1.RDS","wind_NY2.RDS","wind_NY3.RDS","wind_Central_Atlantic_1.RDS", "wind_Central_Atlantic_2.RDS")
+input_shapefile_vals_to_loop2<-c("wind_NY2.RDS")
 
 
 
@@ -24,32 +24,31 @@ rmarkdown::render(here("data_wrangle","combine_cable_routes.Rmd"),
 ##################################################################################
 
 
-# To just render the scallop_analysis_0322.Rmd with whatever parameters are in the markdown, do this:
-# rmarkdown::render(here("analysis_code","scallop_analysis_0322.Rmd"),
-#                   output_file = here("results","scallop_analysis_0322.html"))
-
 
 ##################################################################################
 #Define a function that passes parameters into the rmarkdown::render function
-render_report = function(AA_DAS_only, subtrip_aggregate, input_shapefile) {
-  rmarkdown::render(here("analysis_code","scallop_analysis_0322.Rmd"), params = list(
-      AA_DAS_only = AA_DAS_only,
-      subtrip_aggregate = subtrip_aggregate,
-      input_shapefile=input_shapefile
-    ),
-    output_file = here("results",paste0("scallop_analysis_AA_",AA_DAS_only,"_subtripaggregate_",subtrip_aggregate,"_shapefile_",input_shapefile, ".html")
+render_tiny_report = function(AA_DAS_only, subtrip_aggregate, input_shapefile) {
+  rmarkdown::render(here("analysis_code","scallop_analysis_tiny_report.Rmd"), params = list(
+    AA_DAS_only = AA_DAS_only,
+    subtrip_aggregate = subtrip_aggregate,
+    input_shapefile=input_shapefile
+  ),
+  output_file = here("results",paste0("scallop_tiny_AA_",AA_DAS_only,"_subtripaggregate_",subtrip_aggregate,"_shapefile_",input_shapefile, ".html")
   ))
 }
 ################################################################################## 
 
-# Run lots of scallop analysis reports.
+
+# Run lots of tiny scallop analysis reports.
 for (AA_DAS_only_val in AADAS_Vals_to_loop ) {
   for (subtrip_aggregate_val in subtrip_aggregate_vals_to_loop ) {
-    for (input_shapefile_val in input_shapefile_vals_to_loop)  {
-      render_report(AA_DAS_only_val,subtrip_aggregate_val, input_shapefile_val)
+    for (input_shapefile_val in input_shapefile_vals_to_loop2)  {
+      render_tiny_report(AA_DAS_only_val,subtrip_aggregate_val, input_shapefile_val)
     }
   }
 }
+
+
 
 
 
